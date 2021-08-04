b83a15d4857cdacd39484f120a764c7653e81233
Ramon Ayala <ramon@EntechUS.com>
2021-08-04 16:45:47 -0400

    Changed to use $PSSCriptRoot.

--- SonicwallAPI.psm1
+++ SonicwallAPI.psm1
@@ -1,7 +1,7 @@
 # Importing public and private functions
-$PSScript = "C:\Program Files\WindowsPowerShell\Modules\SonicwallAPI\1.2.0.0"
-$PublicFunc = @(Get-ChildItem -Path $PSScript\Public\*.ps1 -ErrorAction SilentlyContinue)
-$PrivateFunc = @(Get-ChildItem -Path $PSScript\Private\*.ps1 -ErrorAction SilentlyContinue)
+#$PSScript = "C:\Program Files\WindowsPowerShell\Modules\SonicwallAPI\1.2.0.0"
+$PublicFunc = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
+$PrivateFunc = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
 
 # Dotsourcing files
 ForEach ($import in @($PublicFunc + $PrivateFunc)) {
