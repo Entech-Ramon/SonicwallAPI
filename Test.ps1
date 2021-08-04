5e8ab3f2605cf2ee129a1524fcb9f0fbfd6e9707
Ramon Ayala <ramon@EntechUS.com>
2021-08-04 17:25:46 -0400

    Removed unneed variable step.

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,4 @@
-$Creds = [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ("admin", (ConvertTo-SecureString "password" -AsPlainText -Force))
+[pscredential]$Creds = New-Object System.Management.Automation.PSCredential ("admin", (ConvertTo-SecureString "password" -AsPlainText -Force))
 Remove-Module SonicwallAPI
 Import-Module ./SonicwallAPI.psm1
 Connect-SWAppliance -Server '10.0.0.189' -Credential $Creds -Port 443 -Insecure $false
